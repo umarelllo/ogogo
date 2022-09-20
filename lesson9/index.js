ea088a30150e4fc01c4f301e6ff36cc8ae0ee78a
umarelllo <sloppyfinn1603@gmail.com>
2022-09-20 18:42:42 +0600

    lesson9

---
+++ lesson9/index.js
@@ -0,0 +1,14 @@
+const user = {
+    name: 'Sam',
+    age: 22,
+}
+
+console.log(user);
+
+let d = 2;
+console.log(d);
+d++;
+console.log(d);
+
+const ENV_PASSWORD = 1023546;
+const userName = 'beba';
