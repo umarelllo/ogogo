71409b43f0bb8f9f14c1ceb86f8a3a2c176d5a2c
umarelllo <sloppyfinn1603@gmail.com>
2022-09-21 20:07:59 +0600

    lesson-11

---
+++ lesson11/index.js
@@ -0,0 +1,16 @@
+// console.log(window.navigator)
+
+// let obj = {
+//     name: 'Петя',
+// }
+// obj1 = obj
+// let obj1 = JSON.stringfy(obj)
+// obj1 - JSON.parse(obj1)
+// obj.name = 'Саня'
+// console.log(obj1)
+// localStorage.setItem('key', 'Hi')
+// localStorage.getItem('key')
+// localStorage.removeItem('key')
+
+// let myObj = Object.assign({}, obj)
+// console.log(myObj)
